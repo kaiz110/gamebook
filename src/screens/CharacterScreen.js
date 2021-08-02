4f1601a371b9742c7845c10e59497637d0472942
kaiz110 <dinhkhiem110@gmail.com>
2021-08-02 19:21:42 +0700

    char ui

--- src/screens/CharacterScreen.js
+++ src/screens/CharacterScreen.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
-import { Input, Overlay, Button } from 'react-native-elements'
+import { Input, Overlay, Button, Slider } from 'react-native-elements'
 import { useSelector, useDispatch } from 'react-redux'
 import { CHAR_CREATE, CHAR_DEL, CHAR_ATRB } from '../lib/redux/actions/charActions'
 import { PLAY_TEXT } from '../utils/string'
+import { AntDesign } from '@expo/vector-icons'
 
 const CharacterScreen = () => {
     const dispatch = useDispatch()
@@ -12,43 +13,78 @@ const CharacterScreen = () => {
     const [showInput, setShowInput] = useState(false)
     const [name, setName] = useState('')
 
+    const AtrbCard = ({title, value, add}) => (
+        <View style={styles.atrb}>
+            <View style={{flex: 1, alignItems: 'center'}}>
+                <Text style={{fontSize: 16}}>{title}</Text>  
+            </View>
+            
+            <View style={{flex: 2, alignItems: 'center'}}>
+                <Text style={{fontSize: 16}}>{value}</Text>
+            </View>
+            {charState.skillPoint > 0 &&
+            <TouchableOpacity style={{position:'absolute', right: 0}} onPress={add}>
+                <AntDesign name='plus' size={25} />
+            </TouchableOpacity>
+            }
+        </View>
+    )
+
     return <View> 
         {charState != null &&
             <View>
                 <View>
-                    <Text>{TEXT.name}: {charState.name}</Text>
-                    <Text>{TEXT.level}: {charState.lvl}</Text>
-                    <Text>{TEXT.hp}: {charState.hp}</Text>
-                    <Text>{TEXT.atk}: {charState.atk}</Text>
-                    <Text>{TEXT.def}: {charState.def}</Text>
-                    <Text>{TEXT.agi}: {charState.agi}</Text>
-                    <Text>{TEXT.skill_point}: {charState.skillPoint}</Text>
-                    <Text>{TEXT.exp}: {charState.exp} / {charState.lvlupExp}</Text>
-                </View>
+                    <View style={{alignItems: 'center'}}>
+                        <Text style={{fontSize: 35,}}>{charState.name}</Text>
+                        <Text style={{fontSize: 20}}>{TEXT.level}: {charState.lvl}</Text>
+                    </View>
 
-                {charState.skillPoint > 0 &&
-                <View>
-                    <Button
-                        title='tang mau'
-                        onPress={() => dispatch(CHAR_ATRB('hp'))}
-                    />
-                    <Button
-                        title='tang atk'
-                        onPress={() => dispatch(CHAR_ATRB('atk'))}
-                    />
-                    <Button
-                        title='tang def'
-                        onPress={() => dispatch(CHAR_ATRB('def'))}
-                    />
-                    <Button
-                        title='tang agi'
-                        onPress={() => dispatch(CHAR_ATRB('agi'))}
-                    />
-                </View>}
+                    <View>
+                        <Text style={{position: 'absolute', right: 10}}>{charState.exp} / {charState.lvlupExp}</Text>
+
+                        <Slider
+                            trackStyle={{ height: 5 }}
+                            style={{ marginHorizontal: 5 }}
+                            minimumTrackTintColor={'gold'}
+                            thumbStyle={{ height: 0, width: 0 }}
+                            disabled
+                            minimumValue={0}
+                            maximumValue={charState.lvlupExp}
+                            value={charState.exp}
+                        />
+                    </View>
+
+                    <View style={styles.atrbContainer}>
+                        <AtrbCard
+                            title={TEXT.hp}
+                            value={charState.hp}
+                            add={() => dispatch(CHAR_ATRB('hp'))}
+                        />
+                        <AtrbCard
+                            title={TEXT.atk}
+                            value={charState.atk}
+                            add={() => dispatch(CHAR_ATRB('atk'))}
+                        />
+                        <AtrbCard
+                            title={TEXT.def}
+                            value={charState.def}
+                            add={() => dispatch(CHAR_ATRB('def'))}
+                        />
+                        <AtrbCard
+                            title={TEXT.agi}
+                            value={charState.agi}
+                            add={() => dispatch(CHAR_ATRB('agi'))}
+                        />
+                    </View>
+
+                    <Text style={styles.skill}>{TEXT.skill_point}: {charState.skillPoint}</Text>
+                </View>
                 
 
                 <Button
                     title={TEXT.delete_character}
+                    containerStyle={{margin: 50}}
+                    type='clear'
                     onPress={() => dispatch(CHAR_DEL())}
                 />
             </View>
@@ -81,6 +117,27 @@ const CharacterScreen = () => {
     </View>
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    atrb: {
+        flexDirection: 'row', 
+        justifyContent: 'space-around', 
+        alignItems: 'center', 
+        padding: 10, 
+        marginTop: 10,
+        marginHorizontal: 10
+    },
+    atrbContainer: {
+        margin: 10, 
+        borderWidth: 1, 
+        paddingBottom: 10, 
+        borderColor: '#dedede', 
+        borderRadius: 7
+    },
+    skill: {
+        alignSelf: 'flex-end', 
+        marginRight: 10
+    }
+
+})
 
 export default CharacterScreen
