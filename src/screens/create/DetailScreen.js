ce66154234d660ad19b9ba937e86055556c5890c
kaiz110 <dinhkhiem110@gmail.com>
2021-08-16 10:27:29 +0700

    testing add image

--- src/screens/create/DetailScreen.js
+++ src/screens/create/DetailScreen.js
@@ -1,18 +1,22 @@
 import React, { useEffect,useState ,  useLayoutEffect } from 'react'
 import { FlatList } from 'react-native'
-import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity, Alert, Image } from 'react-native'
 import { Divider, Button, Overlay } from 'react-native-elements'
 import { useSelector, useDispatch } from 'react-redux'
 import { DEL_PAGE } from '../../lib/redux/actions/createActions'
 import { PUT_ON } from '../../lib/redux/actions/shelfActions'
 import { Feather } from '@expo/vector-icons'
 import { ADD_ERROR, REMOVE_ERROR } from '../../lib/redux/actions/createActions'
+import * as ImagePicker from 'expo-image-picker'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import * as ImageManipulator from 'expo-image-manipulator';
 
 const DetailScreen = ({navigation,route}) => {
     const dispatch = useDispatch()
     const state = useSelector(reducer => reducer.create)
     const book = state.books.find(val => val.name === state.currentBook)
     const [showUpload, setShowUpload] = useState(false)
+    const [previewImage, setPreviewImage] = useState(null)
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -49,12 +53,46 @@ const DetailScreen = ({navigation,route}) => {
         }
     }, [ book.story ])
 
+    const test = async () => {
+        const result = await AsyncStorage.getItem('@test')
+        console.log('result', result.slice(-50))
+        setPreviewImage(result)
+    }
+
+    const openImage = async () => {
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+        if(permissionResult.granted == true) {
+            let pickResult = await ImagePicker.launchImageLibraryAsync({base64: true})
+            const resizePick = await ImageManipulator.manipulateAsync(
+                `data:image/jpg;base64,${pickResult.base64}`,
+                [{resize: {width: 370, height: 270}}],
+                { compress: 0.75, format: 'jpeg', base64: true}
+            )
+            console.log('BASE64', resizePick.base64.length)
+            setPreviewImage(resizePick.base64)
+            await AsyncStorage.setItem('@test', resizePick.base64)
+        }
+    }
+
     const listHeader = () => (
         <View>
+            {previewImage != null &&
+            <Image source={{uri: `data:image/jpeg;base64,${previewImage}`}} style={{width: 290, height: 175, alignSelf: 'center'}}/>
+            }
             <Text style={{alignSelf: 'center', fontSize: 24}}>{book.name}</Text>
             <View style={{borderWidth: 1, width: '100%', margin: 10, borderColor: '#dedede', padding: 10}}>
                 <Text>{book.summary}</Text>
             </View>
+
+            <Button
+                title='add image'
+                onPress={openImage}
+            />
+            <Button
+                title='get image'
+                onPress={test}
+            />
 
             <Button
                 title='Characters'
