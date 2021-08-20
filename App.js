d8753595e7ef3e180243b91cc748e5c276ab3ddb
kaiz110 <dinhkhiem110@gmail.com>
2021-08-20 18:17:08 +0700

    add redux persist

--- App.js
+++ App.js
@@ -2,16 +2,28 @@ import 'react-native-gesture-handler'
 import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 import reducer from './src/lib/redux/reducers'
 import Route from './src/navigation'
-import HomeScreen from './src/screens/play/HomeScreen'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const reduxStore = createStore(reducer)
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+const store = createStore(persistedReducer)
+const persistor = persistStore(store)
 
 export default () => 
 (
-  <Provider store={reduxStore}>
-    <Route/>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Route/>
+    </PersistGate>
   </Provider>
 )
 
